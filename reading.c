411e52f8b03d1792501feaa0029b66574c2ffb1b
Mouad Kimdil <mkimdil@e3r1p3.1337.ma>
2024-03-01 07:41:34 +0100

    fix norminette need to add 42 header

--- reading.c
+++ reading.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   reading.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abouafso <abouafso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mkimdil <mkimdil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:01:14 by abouafso          #+#    #+#             */
-/*   Updated: 2024/02/28 03:57:26 by abouafso         ###   ########.fr       */
+/*   Updated: 2024/03/01 07:35:40 by mkimdil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-char **read_map(int fd, char *av)
+char	**read_map(int fd, char *av)
 {
-	int i;
-	char *line;
-	char **all_lines;
+	int		i;
+	char	*line;
+	char	**all_lines;
 
 	i = count_lines(fd);
 	close(fd);
-	fd = open(av , O_RDONLY);
+	fd = open(av, O_RDONLY);
 	all_lines = (char **)malloc(sizeof(char *) * i);
 	if (!all_lines)
 		return (NULL);
 	i = 0;
-	while(1)
+	while (1)
 	{
 		line = get_next_line(fd);
 		if (!line)
@@ -33,40 +33,25 @@ char **read_map(int fd, char *av)
 		all_lines[i] = ft_strdup(line);
 		free(line);
 		if (!all_lines[i])
-			break;
+			break ;
 		i++;
 	}
 	all_lines[i] = NULL;
-	return (all_lines);	
+	return (all_lines);
 }
 
-int count_lines(int fd)
+int	count_lines(int fd)
 {
-	int count;
-	char *line;
-	
+	int		count;
+	char	*line;
+
 	count = 0;
-	while(1)
+	while (1)
 	{
 		line = get_next_line(fd);
 		if (line == NULL)
-			break;
+			break ;
 		count++;
 	}
-	return(count);
+	return (count);
 }
-
-
-// int main()
-// {
-// 	int fd = open("map.ber", O_RDONLY);
-// 	char *line = get_next_line(fd);
-// 	char **lines = read_map(fd, line);
-// 	while (line)
-// 	{
-// 		// line;
-// 		free(line);
-// 		line = get_next_line(fd);
-				
-// 	}
-// }
--- so_long.c
+++ so_long.c
@@ -1,12 +1,13 @@
 #include "so_long.h"
 
-int main (int ac, char **av)
+int	main(int ac, char **av)
 {
 	t_list	vars;
+
 	if (ac != 2)
 		ft_error("Error: Invalid number of arguments\n");
-	vars.C = 0;
-	vars.E = 0;
-	vars.P = 0;
+	vars.c = 0;
+	vars.e = 0;
+	vars.p = 0;
 	parsing(av, &vars);
 }
